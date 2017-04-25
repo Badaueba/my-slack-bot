b0ddb87b2dd44989bb10dbf1de6b1a2780952f11
badaueba <alan.develop@outlook.com>
2017-04-25 12:08:50 -0300

    bot challenge

--- index.js
+++ index.js
@@ -1,101 +1,53 @@
-//enable strict mode, use es6 sintax, such let, const...
 'use strict'
+let token_config = require('./token_config');
+let Bot = require('./bot');
 
-const RtmClient = require('@slack/client').RtmClient;
-
-const MemoryDataScore = require('@slack/client').MemoryDataStore;
-
-const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-
-const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
-
-const token = require('./token_config').token;
-
-let slack = new RtmClient(token, {
-    logLevel : 'error',
-    dataStore : new MemoryDataScore(),
+const bot = new Bot({
+    token : token_config.token,
     autoReconnect : true,
     autoMark : true
-})
+});
 
-slack.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
-    let user = slack.dataStore.getUserById(slack.activeUserId);
+bot.respondTo('hello', (message, channel, user) => {
+    bot.send(`Hello to you too, ${user.name}!`, channel)
+}, true);
 
-    let team = slack.dataStore.getTeamById(slack.activeTeamId);
+bot.respondTo('roll', (message, channel, user) => {
+    const members = bot.getMembersByChannel(channel);
 
-    console.log(`Connected to ${team.name} as ${user.name}`);
-
-    let channels = getChannels(slack.dataStore.channels);
-
-    let channelNames = channels.map( (channel) => {
-        return channel.name
-    }).join(', ');
-    console.log(`Currently in : ${channelNames}`)
-    
-    channels.forEach((channel) => {
-        let members = channel.members.map( (id) => {
-            return slack.dataStore.getUserById(id);
-        })
-        members = members.filter((member) => {
-            return !member.is_bot;
-        });
-        
-        let membersNames = members.map( (member) => {
-            return member.name;
-        }).join(', ');
-
-        console.log('Members of this channel: ', membersNames)
-    });
-
-})
-
-slack.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
-    console.log(`Logged in as 
-        ${rtmStartData.self.name} of team 
-        ${rtmStartData.team.name}, but not yet connected to a channel`)
-})
-
-slack.on(RTM_EVENTS.MESSAGE, (message) => {
-    let user = slack.dataStore.getUserById(message.user)
-    if (user && user.is_bot) {
-        return
+    if (!members) {
+        bot.send('You have to challenge someone in a channel, not a direct message!', channel);
+        return;
     }
-    let channel = slack.dataStore.getChannelGroupOrDMById(message.channel);
 
-    if (message.text) {
-        let msg = message.text.toLowerCase();
-        
-        //CUSTOMIZE IT 
-        if (/uptime/g.test(msg)) {
-            let dm = slack.dataStore.getDMByName(user.name);
-            let uptime = process.uptime();
-            
-            let minutes = parseInt(uptime / 60, 10),
-                hours = parseInt(minutes / 60, 10),
-                seconds = parseInt(uptime - (minutes * 60) - ((hours * 60) * 60), 10)
+    let args = getArgs(message.text);
 
-            slack.sendMessage(`I have running for: ${hours} hours, ${minutes} minutes and ${seconds} seconds.`, dm.id)
-        }
-
-        if (/(oi|ola|e ai|eae) (bot|amigao|cao)/g.test(msg)) {
-            slack.sendMessage(`Oi pra vc tbm, ${user.name}!`, channel.id)
-        }
+    if (args.length < 1) {
+        bot.send('You have to provide the name of the person you wish to challenge!', channel);
+        return;
     }
-})
 
-
-slack.start();
-
-//return an array of all the channels the bot resides in
-function getChannels(allChannels) {
-    let channels = []
-
-    for (let id in allChannels) {
-        let channel = allChannels[id]
-        console.log(channel.name)
-        if (channel.is_member){
-            channels.push(channel)
-        }
-        return channels;
+    if (members.indexOf(args[0]) < 0) {
+        bot.send(`Sorry ${user.name}, but i either can't find ${args[0]} in this channel, or they are a bot!`, channel);
+        return;
     }
+    let firstRoll = Math.round(Math.random() * 100);
+    let secondRoll = Math.round(Math.random() * 100);
+    while (firstRoll === secondRoll) {
+        secondRoll = Math.round(Math.random() * 100);
+    }
+    let challenger = user.name;
+    let opponent = args[0];
+    let winner = firstRoll > secondRoll ? challenger : opponent;
+
+    bot.send(`${challenger} fancies their changes against ${opponent}!\n
+        ${challenger} rolls: ${firstRoll}\n
+        ${opponent} rolls: ${secondRoll}\n\n
+        *${winner} is the winner!*`, channel
+    );
+
+}, true)
+
+function getArgs (msg) {
+    return msg.split(' ').slice(1)
 }
